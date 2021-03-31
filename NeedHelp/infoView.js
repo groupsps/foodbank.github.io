3251336fd0002107e0196394eb9e72a9b8a8d2f8
Adrian Salgado Lopez <57457579+sa2urn@users.noreply.github.com>
2021-03-31 13:10:51 -0700

    Move functions around, update createMap() to add markers to map.

--- NeedHelp/infoView.js
+++ NeedHelp/infoView.js
@@ -7,6 +7,57 @@ function mapView(){
   button1.classList.toggle('active');
   createMap();
 }
+function createMap() {
+  const laLat = 34;
+  const laLng = -118.25;
+  const laLoc = {lat: laLat, lng: laLng}
+  const mapStyle = {
+      zoom: 11.5,
+      center: laLoc,
+      mapId: "892b95989cdc9cc6",
+  };
+  const map = new google.maps.Map(
+    document.getElementById('map'),
+    mapStyle
+  );
+
+  fetch('resources.json')
+    .then(function(resp){
+      return resp.json();
+    })
+    .then(function(data){
+      for(num = 0; num < data.size; num++){
+        const pantryLocation = {lat: data.foodPantries[num].latitude, lng: data.foodPantries[num].longitude};
+        const marker = new google.maps.Marker({
+          map: map,
+          position: pantryLocation,
+          title: data.foodPantries[num].name
+        })
+
+        const name = data.foodPantries[num].name;
+        const street = data.foodPantries[num].street;
+        const zipCityState = data.foodPantries[num].cityState + ' ' + data.foodPantries[num].zipCode;
+        const description = data.foodPantries[num].description;
+        const content = '<div class="infoWindow">' + 
+        '<p class="infoWindow-title"><strong>' + name + '</strong></p>' + 
+        '<p>' + street + '</p>' +
+        '<p>' + zipCityState + '</p>' +
+        '<br>' + 
+        '<p class="infoWindow-title"><b> Food Distripution: </b></p>' +
+        '<p>' + description + '</p>' +
+        '</div';
+        const infoWindow = new google.maps.InfoWindow({
+          content: content,
+        })
+
+        marker.addListener("click", ()=>{
+          map.setZoom(17);
+          map.setCenter(marker.getPosition());
+          infoWindow.open(map, marker)
+        })
+      }
+    })
+}
 
 function listView(){
   const button2 = document.querySelector('.button-2');
@@ -20,21 +71,6 @@ function listView(){
   for(indx = 0; indx < resourceNames.length; indx++){
     resourceNames[indx].classList.toggle('active');
   }
-}
-
-function createMap() {
-  const laLat = 34;
-  const laLng = -118.25;
-  const laLoc = {lat: laLat, lng: laLng}
-  const mapStyle = {
-      zoom: 10.1,
-      center: laLoc,
-      mapId: "892b95989cdc9cc6",
-  };
-  const map = new google.maps.Map(
-    document.getElementById('map'),
-    mapStyle
-  );
 }
 
 function addPantries(){
